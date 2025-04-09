f55193dd0fc475a4ecf283fef19a28f799c9a687
seyukun <yus-sato@kalyte.ro>
2025-04-09 23:57:10 +0900

    wip

--- src/actions/isLoggedIn.ts
+++ src/actions/isLoggedIn.ts
@@ -2,22 +2,17 @@
 
 import { PrismaClient } from "@prisma/client";
 import { cookies } from "next/headers";
-import { Logger } from "tslog";
 
 import isExist from "@/helper/isExist";
 import Session from "@/lib/session";
 
 const prisma = new PrismaClient();
 
-const console = new Logger();
-
 export async function isLoggedIn() {
   const cookieStore = await cookies();
   console.debug(isLoggedIn.name, "cookies before", cookieStore.getAll());
   const session = new Session(cookieStore);
   const sessval = await session.start();
-  console.debug(isLoggedIn.name, "cookies after", cookieStore.getAll());
-  console.debug(sessval);
   const userId = sessval["userid"];
 
   const user = isExist(userId)
